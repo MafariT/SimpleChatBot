01b3832a33e8bf21feedcca967cc70205fde47f5
MafariT <ahmadfadlan0896@gmail.com>
2023-03-19 02:59:44 +0700

    added logger settings

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,19 @@
 import logging
+from src.config import LOGGING_ENABLED
 
 def setup_logger(name, log_file, level=logging.DEBUG):
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler = logging.FileHandler(log_file)
-    handler.setFormatter(formatter)
+    if LOGGING_ENABLED:
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(formatter)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(handler)
-
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.addHandler(handler)
+    else:
+        class NullHandler(logging.Handler):
+            def emit(self, record):
+                pass
+        logger = logging.getLogger(__name__)
+        logger.addHandler(NullHandler())
     return logger
