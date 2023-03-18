668757ebb514f360ead35451424f97765cf400c8
MafariT <ahmadfadlan0896@gmail.com>
2023-03-18 10:17:50 +0700

    removing tts

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 # Importing required libraries
 import random
 import time
-import pyttsx3
 from textblob import TextBlob
 
-# Initializing text-to-speech engine
-engine = pyttsx3.init()
 
 # Defining the responses
 responses = {
     "hello": ["Hi there!", "Hello!", "Greetings!"],
     "how are you": ["I'm doing well, thank you.", "I'm fine, thanks for asking.", "I'm good, how about you?"],
     "what's your name": ["My name is Chatbot.", "I'm Chatbot, nice to meet you.", "I go by the name Chatbot."],
-    "bye": ["Goodbye!", "See you later!", "Take care!"]
+    "bye": ["Goodbye!", "See you later!", "Take care!"],
+    "thank you": ["You're welcome!", "No problem!", "Anytime!"],
+    "what time is it": ["I'm sorry, I don't have a clock.", "I'm not sure, sorry.", "I don't know, sorry."],
+    "how old are you": ["I don't have an age, I'm a chatbot!", "I'm ageless, I'm a chatbot!", "I don't age, I'm a chatbot!"],
+    "what's the weather like": ["I'm sorry, I don't have access to weather information.", "I'm not sure, sorry.", "I don't know, sorry."],
 }
 
 # Defining the chatbot function
 def chatbot():
     # Greeting message
     print("Hi, I'm a chatbot. What can I help you with today?")
-    
-    # Text-to-speech enabled by default
-    tts_enabled = False
     
     # Chatting loop
     while True:
@@ -32,27 +30,16 @@ def chatbot():
         if user_input == "bye":
             response = random.choice(responses["bye"])
             print(response)
-            if tts_enabled:
-                engine.say(response)
-                engine.runAndWait()
-            break
         
         # Checking for matching responses
         for key in responses.keys():
             if user_input in key:
                 response = random.choice(responses[key])
                 print(response)
-                if tts_enabled:
-                    engine.say(response)
-                    engine.runAndWait()
-                break
         else:
             # Random response for unknown input
             response = random.choice(["I'm sorry, I didn't understand that.", "Could you please rephrase that?", "I'm not sure what you mean."])
             print(response)
-            if tts_enabled:
-                engine.say(response)
-                engine.runAndWait()
             
         # Time delay
         time.sleep(1)
@@ -63,15 +50,9 @@ def chatbot():
         if sentiment > 0.5:
             response = "That's great to hear!"
             print(response)
-            if tts_enabled:
-                engine.say(response)
-                engine.runAndWait()
         elif sentiment < -0.5:
             response = "I'm sorry to hear that."
             print(response)
-            if tts_enabled:
-                engine.say(response)
-                engine.runAndWait()
         
         # Enable/disable text-to-speech
         if user_input == "disable tts":
