4efdd3e432c9afa86eaa02ec4183f282a1468517
MafariT <ahmadfadlan0896@gmail.com>
2023-03-19 12:00:58 +0700

    added error handler on translate

--- src/bot.py
+++ src/bot.py
@@ -32,19 +32,23 @@ def translate_text(text, target_language):
     return translated_text
 
 def translate_text_from_input(lemmas):
-    # Get the text to translate and the target language
-    translate_index = lemmas.index("translate")
-    text_to_translate = " ".join(lemmas[translate_index+1:])
-    target_language_index = lemmas.index("to") if "to" in lemmas else -1
-    if target_language_index != -1:
-        target_language = lemmas[target_language_index+1]
-        text_to_translate = " ".join(lemmas[translate_index+1:target_language_index])
-    else:
-        target_language = "en"
-    # Translate the text
-    translated_text = translate_text(text_to_translate, target_language=target_language)
+    try:
+        # Get the text to translate and the target language
+        translate_index = lemmas.index("translate")
+        text_to_translate = " ".join(lemmas[translate_index+1:])
+        target_language_index = lemmas.index("to") if "to" in lemmas else -1
+        if target_language_index != -1:
+            target_language = lemmas[target_language_index+1]
+            text_to_translate = " ".join(lemmas[translate_index+1:target_language_index])
+        else:
+            target_language = "en"
+        # Translate the text
+        translated_text = translate_text(text_to_translate, target_language=target_language)
+    except Exception as e:
+        logger.error(f"Error occurred while translating text: {text_to_translate}", exc_info=True)
+        translated_text = f"An error occurred while translating the text: '{text_to_translate}'. Please check the log file for more information on the error."
     return translated_text
-
+    
 def get_day_info():
     now = datetime.datetime.now()
     response = f"Today is {now.strftime('%A, %B %d, %Y')}."
