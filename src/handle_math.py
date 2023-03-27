8d29189b9c996245623da39c3bc1250d7f4e674e
MafariT <ahmadfadlan0896@gmail.com>
2023-03-27 15:27:26 +0700

    refactor

--- src/handle_math.py
+++ src/handle_math.py
@@ -3,60 +3,81 @@ import ast
 from src.logger import logger
 
 
-def evaluate_expression(node):
-    if isinstance(node, ast.Num):
-        return node.n
+class UnsupportedNodeTypeError(TypeError):
+    pass
 
-    if isinstance(node, ast.BinOp):
-        left = evaluate_expression(node.left)
-        right = evaluate_expression(node.right)
+class UnsupportedFunctionError(TypeError):
+    pass
+
+class UnsupportedOperatorError(TypeError):
+    pass
+
+
+class ExpressionEvaluator:
+    OPERATORS = {
+        ast.Add: lambda left, right: left + right,
+        ast.Sub: lambda left, right: left - right,
+        ast.Mult: lambda left, right: left * right,
+        ast.Div: lambda left, right: left / right,
+        ast.Mod: lambda left, right: left % right,
+        ast.Pow: lambda left, right: left ** right,
+    }
+
+    FUNCTIONS = {
+        'sqrt': math.sqrt,
+        'sin': math.sin,
+        'cos': math.cos,
+        'tan': math.tan,
+        'log': lambda x, y=None: math.log(x, y),
+    }
+
+    def evaluate(self, node):
+        if isinstance(node, ast.Num):
+            return node.n
+        elif isinstance(node, ast.BinOp):
+            return self._evaluate_binop(node)
+        elif isinstance(node, ast.Call):
+            return self._evaluate_call(node)
+        else:
+            raise UnsupportedNodeTypeError(f"Unsupported node type: {type(node)}")
+
+    def _evaluate_binop(self, node):
+        left = self.evaluate(node.left)
+        right = self.evaluate(node.right)
         operator = node.op
-
-        if isinstance(operator, ast.Add):
-            return left + right
-        elif isinstance(operator, ast.Sub):
-            return left - right
-        elif isinstance(operator, ast.Mult):
-            return left * right
-        elif isinstance(operator, ast.Div):
-            return left / right
-        elif isinstance(operator, ast.Mod):
-            return left % right
-        elif isinstance(operator, ast.Pow):
-            return left ** right
+        op_func = self.OPERATORS.get(type(operator))
+        if op_func is not None:
+            return op_func(left, right)
         else:
-            raise TypeError(f"Unsupported operator: {operator}")
+            raise UnsupportedOperatorError(f"Unsupported operator: {operator}")
 
-    if isinstance(node, ast.Call):
+    def _evaluate_call(self, node):
         func = node.func.id
-        args = [evaluate_expression(arg) for arg in node.args]
-
-        if func == 'sqrt':
-            return math.sqrt(args[0])
-        elif func == 'sin':
-            return math.sin(args[0])
-        elif func == 'cos':
-            return math.cos(args[0])
-        elif func == 'tan':
-            return math.tan(args[0])
-        elif func == 'log':
-            return math.log(args[0], args[1] if len(args) > 1 else None)
+        args = [self.evaluate(arg) for arg in node.args]
+        func_func = self.FUNCTIONS.get(func)
+        if func_func is not None:
+            return func_func(*args)
         else:
-            raise TypeError(f"Unsupported function: {func}")
-
-    raise TypeError(f"Unsupported node type: {type(node)}")
+            raise UnsupportedFunctionError(f"Unsupported function: {func}")
 
 
 def get_math_calc(user_input):
     try:
         expression = user_input.split('math ')[1]
         node = ast.parse(expression, mode='eval')
-        result = evaluate_expression(node.body)
+        evaluator = ExpressionEvaluator()
+        result = evaluator.evaluate(node.body)
         response = f"The result of the calculation is {result}!"
     except SyntaxError:
         response = "Invalid expression"
+    except UnsupportedNodeTypeError as e:
+        response = str(e)
+    except UnsupportedFunctionError as e:
+        response = str(e)
+    except UnsupportedOperatorError as e:
+        response = str(e)
     except Exception as e:
         logger.error(f"Error occurred while performing calculation: {expression}", exc_info=True)
         response = "Sorry, I couldn't perform that calculation. Please check the log file for more information on the error."
-    
+
     return response
