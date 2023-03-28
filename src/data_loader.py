d17c9ffd6501348fafe6a6650f67b00f50e78602
MafariT <ahmadfadlan0896@gmail.com>
2023-03-28 07:07:05 +0700

    refactor

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,40 +1,25 @@
 import json
 import threading
 
-
-def load_data(key: dict) -> dict:
+# Define a function to load data from a JSON file
+def load_data(key: str) -> dict:
     with open('src/data/chat_data.json', encoding='utf-8') as f:
         data = json.load(f)[key]
     return data
 
-general_responses_thread = threading.Thread(target=lambda: load_data('general_responses'))
-default_responses_thread = threading.Thread(target=lambda: load_data('default_responses'))
-excluded_words_thread = threading.Thread(target=lambda: load_data('excluded_words'))
-good_sentiment_responses_thread = threading.Thread(target=lambda: load_data('good_sentiment_responses'))
-bad_sentiment_responses_thread = threading.Thread(target=lambda: load_data('bad_sentiment_responses'))
-help_math_thread = threading.Thread(target=lambda: load_data('help_math'))
-help_general_thread = threading.Thread(target=lambda: load_data('help_general'))
+# Define a list of data keys to load
+data_keys = ['general_responses', 'default_responses', 'excluded_words', 
+            'good_sentiment_responses', 'bad_sentiment_responses', 
+            'help_math', 'help_general']
 
-general_responses_thread.start()
-default_responses_thread.start()
-excluded_words_thread.start()
-good_sentiment_responses_thread.start()
-bad_sentiment_responses_thread.start()
-help_math_thread.start()
-help_general_thread.start()
+# Create a list of threads to load data in parallel
+threads = [threading.Thread(target=lambda key=key: load_data(key)) for key in data_keys]
 
-general_responses_thread.join()
-default_responses_thread.join()
-excluded_words_thread.join()
-good_sentiment_responses_thread.join()
-bad_sentiment_responses_thread.join()
-help_math_thread.join()
-help_general_thread.join()
+# Start and join all threads
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
 
-general_responses = load_data('general_responses')
-default_responses = load_data('default_responses')
-excluded_words = load_data('excluded_words')
-good_sentiment_responses = load_data('good_sentiment_responses')
-bad_sentiment_responses = load_data('bad_sentiment_responses')
-help_math = load_data('help_math')
-help_general = load_data('help_general')
+# Load data from each key into separate variables
+general_responses, default_responses, excluded_words, good_sentiment_responses, bad_sentiment_responses, help_math, help_general = [load_data(key) for key in data_keys]
