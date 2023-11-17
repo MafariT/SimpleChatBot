010a002697b771625ee6931eab424245448e21ab
MafariT <ahmadfadlan0896@gmail.com>
2023-11-17 18:23:46 +0700

    refactor

--- src/handle_translate.py
+++ src/handle_translate.py
@@ -1,21 +1,26 @@
 from deep_translator import GoogleTranslator
 from src.logger import logger
 
-
 def translate_text_from_input(lemmas: str) -> str:
     try:
-        # Get the text to translate and the target language
+        # Extract text to translate and target language from lemmas
         translate_index = lemmas.index("translate")
-        text_to_translate = " ".join(lemmas[translate_index+1:])
+        text_to_translate = " ".join(lemmas[translate_index + 1:])
+
         target_language_index = len(lemmas) - lemmas[::-1].index("to") - 1 if "to" in lemmas else -1
         if target_language_index != -1:
-            target_language = lemmas[target_language_index+1]
-            text_to_translate = " ".join(lemmas[translate_index+1:target_language_index])
+            target_language = lemmas[target_language_index + 1]
+            text_to_translate = " ".join(lemmas[translate_index + 1:target_language_index])
         else:
             target_language = "en"
+
         # Translate the text
-        translated_text = GoogleTranslator(source='auto', target=target_language).translate(text_to_translate)
+        translator = GoogleTranslator(source='auto', target=target_language)
+        translated_text = translator.translate(text_to_translate)
+
     except Exception as e:
+        # Log and handle translation errors
         logger.error(f"Error occurred while translating text: {text_to_translate}. Exception: {e}", exc_info=True)
         translated_text = f"An error occurred while translating the text: '{text_to_translate}'. Please check the log file for more information on the error"
+
     return translated_text
--- src/handle_weather.py
+++ src/handle_weather.py
@@ -14,33 +14,29 @@ def get_forecast(lat: float, lon: float) -> dict:
     response = requests.get(url, timeout=10)
 
     if response.status_code == 200:
-        data = response.json()
-        return data
+        return response.json()
     else:
         return None
 
 def format_temperature_data(data: dict) -> tuple:
     temperature_data = []
     time_data = []
-    for entry in data['list'][:8]:
-        temperature = entry['main']['temp']
-        timestamp = entry['dt']
+    for forecast_entry in data['list'][:8]:
+        temperature = forecast_entry['main']['temp']
+        timestamp = forecast_entry['dt']
         temperature_data.append(temperature)
-        time_data.append(timestamp)
-
-    # Convert the timestamps to datetime objects
-    time_data = [datetime.datetime.fromtimestamp(timestamp) for timestamp in time_data]
+        time_data.append(datetime.datetime.fromtimestamp(timestamp))
 
     return time_data, temperature_data
 
 def format_weather_data(data: dict) -> list:
     weather_data = []
-    for entry in data['list']:
-        timestamp = entry['dt']
+    for forecast_entry in data['list']:
+        timestamp = forecast_entry['dt']
         date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-        temperature = entry['main']['temp']
-        humidity = entry['main']['humidity']
-        description = entry['weather'][0]['description']
+        temperature = forecast_entry['main']['temp']
+        humidity = forecast_entry['main']['humidity']
+        description = forecast_entry['weather'][0]['description']
         weather_data.append((date, temperature, humidity, description))
 
     return weather_data
@@ -48,9 +44,7 @@ def format_weather_data(data: dict) -> list:
 def group_weather_data(weather_data: list) -> dict:
     grouped_weather_data = {}
     for date, temperature, humidity, description in weather_data:
-        if date not in grouped_weather_data:
-            grouped_weather_data[date] = []
-        grouped_weather_data[date].append((temperature, humidity, description))
+        grouped_weather_data.setdefault(date, []).append((temperature, humidity, description))
 
     return grouped_weather_data
 
@@ -66,13 +60,13 @@ def get_weather_info() -> str:
             grouped_weather_data = group_weather_data(weather_data)
 
             # Format the weather information into a response
-            response = f"\nThe current temperature is {temperature_data[0]} degrees Celsius, The weather is {weather_data[0][3]} in {city}\n\n"
+            response = f"\nThe current temperature is {temperature_data[0]} degrees Celsius. The weather is {weather_data[0][3]} in {city}\n\n"
             response += f"Here's the temperature forecast for the next 24 hours in {city}:\n\n"
             response += "- " + "\n- ".join([f"{time_data[i].strftime('%Y-%m-%d %H:%M:%S')}: {weather_data[i][3]}, {temperature_data[i]} degrees Celsius, " for i in range(len(time_data))])
             response += f"\n\nHere's the weather forecast for the next 5 days in {city}:\n"
             for date, weather_list in grouped_weather_data.items():
-                average_temperature = sum([t for t, h, d in weather_list]) / len(weather_list)
-                average_humidity = sum([h for t, h, d in weather_list]) / len(weather_list)
+                average_temperature = sum([t for t, h, _ in weather_list]) / len(weather_list)
+                average_humidity = sum([h for _, h, _ in weather_list]) / len(weather_list)
                 description = weather_list[0][2]
                 response += f"\n{date}: {description}, {average_temperature:.1f} degrees Celsius, {average_humidity:.0f}% humidity"
         else:
@@ -82,5 +76,6 @@ def get_weather_info() -> str:
         response = "An error occurred while retrieving the weather information. Please check the log file for more information on the error"
 
     return response
+
 
 
